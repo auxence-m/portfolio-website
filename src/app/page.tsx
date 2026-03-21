10879ec2d87e062bcaa76077b91539abc1340f19
Auxence-M <auxencemedja@gmail.com>
2026-03-21 18:55:27 -0400

    Initial commit from Create Next App

---
+++ src/app/page.tsx
@@ -0,0 +1,36 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col min-h-screen flex-1 items-center justify-center bg-zinc-50 dark:bg-black">
+      <div className="flex gap-y-20 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+        <Image
+          className="dark:invert"
+          src="/next.svg"
+          alt="Next.js logo"
+          width={100}
+          height={20}
+          priority
+        />
+        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
+          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
+            To get started, edit the page.tsx file.
+          </h1>
+          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
+            Looking for a starting point or more instructions? Check out our documentation
+          </p>
+        </div>
+        <div className="text-base font-medium sm:flex-row">
+          <a
+            className="flex h-12 w-full items-center justify-center rounded-full border border-solid text-black dark:text-zinc-50 border-black/8 px-5 transition-colors hover:border-transparent hover:bg-black/4 dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-39.5"
+            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Documentation
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
