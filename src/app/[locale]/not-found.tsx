4efb66961a8e1067347e6131f2ccc9b2248081e1
Auxence-M <auxencemedja@gmail.com>
2026-03-29 02:20:49 -0400

    Added a temporary not found page

---
+++ src/app/[locale]/not-found.tsx
@@ -0,0 +1,10 @@
+
+export default function NotFound() {
+    return (
+        <main className="flex flex-1 items-center min-h-screen pt-20 justify-center bg-surface-container-lowest text-on-surface">
+            <div className="text-on-surface">
+                NOT FOUND
+            </div>
+        </main>
+    );
+}
