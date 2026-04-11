32400caed042cffac6761c4ee5b3ce46b24a052a
Auxence-M <auxencemedja@gmail.com>
2026-04-10 23:40:18 -0400

    Moved socials object to utils for reusability

--- src/components/HomeCTA.tsx
+++ src/components/HomeCTA.tsx
@@ -4,19 +4,9 @@ import {useTranslations} from "next-intl";
 import {ArrowRight} from "lucide-react";
 import Image from "next/image";
 import {Link} from "@/i18n/navigation";
+import {socialMedias} from "@/lib/utils";
 
-const socials = [
-    {
-        name: "GitHub",
-        alt: "GitHub logo",
-        logo: "/github-logo.svg",
-    },
-    {
-        name: "LinkedIn",
-        alt: "LinkedIn logo",
-        logo: "/linkedin-logo.svg",
-    },
-]
+
 
 export default function HomeCTA() {
     const t = useTranslations("CallToAction");
@@ -38,8 +28,8 @@ export default function HomeCTA() {
                 </div>
                 <div className="mt-12 flex justify-center gap-10">
                     {
-                        socials.map((social) => (
-                            <a href="#" key={social.name} className="inline-flex items-center justify-center gap-4 text-on-surface-variant group font-bold tracking-[0.2em] uppercase hover:text-primary">
+                        socialMedias.map((social) => (
+                            <a target="_blank" rel="noopener noreferrer" href={social.href} key={social.name} className="inline-flex items-center justify-center gap-4 text-on-surface-variant group font-bold tracking-[0.2em] uppercase hover:text-primary transition-colors duration-300">
                                 <div className="flex items-center shrink-0 justify-center p-2 border border-outline-variant rounded-sm group-hover:border-primary transition-all duration-300">
                                     <Image alt={social.alt} src={social.logo} width={128} height={128} className="size-6"/>
                                 </div>
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,21 @@
+export const socialMedias = [
+    {
+        name: "GitHub",
+        alt: "GitHub logo",
+        logo: "/github-logo.svg",
+        href: "https://github.com/auxence-m",
+        alias: "auxence-m"
+    },
+    {
+        name: "LinkedIn",
+        alt: "LinkedIn logo",
+        logo: "/linkedin-logo.svg",
+        href: "https://www.linkedin.com/in/auxence-medja/",
+        alias: "Auxence MEDJA"
+    },
+]
+
+
 export function scrollToSection(id: string) {
     const element = document.getElementById(id);
     if (element) {
